2811771124e260c6f44d1c50ab8acd685fd0fcba
Kamil Wójcik <Kamil Wjcik>
2017-04-06 23:02:17 +0200

    Set proxy, base routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RoutingModule } from './routing.module';
 import { AppComponent } from './app.component';
 import { UserGameComponent } from './user-game/user-game.component';
 
@@ -14,7 +14,8 @@ import { UserGameComponent } from './user-game/user-game.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { UserGameComponent } from './user-game/user-game.component';
+
+export const router: Routes = [
+	{ path: 'user-game', component: UserGameComponent}	
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(router)],
+  exports: [RouterModule]
+})
+export class RoutingModule {}
