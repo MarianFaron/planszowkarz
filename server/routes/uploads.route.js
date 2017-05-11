71dd35bb4dfc908e069bd4e496f833f28a51bded
xmarianF <xmarianF@gmail.com>
2017-05-11 17:00:25 +0200

    Upload files to src and dist folders

--- server/routes/uploads.route.js
+++ server/routes/uploads.route.js
@@ -3,8 +3,16 @@ var router = express.Router();
 var multer = require('multer');
 var rootUrl = 'localhost:8080';
 
+var storage_src = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../src/assets/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
 
-var storage = multer.diskStorage({
+var storage_dist = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, '../dist/assets/uploads/')
   },
@@ -13,12 +21,20 @@ var storage = multer.diskStorage({
   }
 });
 
-var upload = multer({ storage: storage }).single('photo');
+var upload_to_src = multer({ storage: storage_src }).single('photo');
+var upload_to_dist = multer({ storage: storage_dist }).single('photo');
 
 router.route('/uploads')
   .post(function(req, res, next) {
    var path = '';
-   upload(req, res, function (err) {
+   upload_to_src(req, res, function (err) {
+      if (err) {
+        console.log(err);
+        return res.status(422).send("an Error occured")
+      }
+      path = req.file.path;
+  });
+  upload_to_dist(req, res, function (err) {
       if (err) {
         console.log(err);
         return res.status(422).send("an Error occured")
