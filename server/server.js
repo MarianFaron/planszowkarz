3a20208f6a9c89dcbc078d4293d79a6a0ca174c0
xmarianF <xmarianF@gmail.com>
2017-06-03 15:41:39 +0200

    change forceSSL in server to http

--- server/server.js
+++ server/server.js
@@ -68,9 +68,9 @@ app.use('/app', notificationsRoute);
 
 const forceSSL = function() {
   return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
+    if (req.headers['x-forwarded-proto'] !== 'http') {
       return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
+       ['http://', req.get('Host'), req.url].join('')
       );
     }
     next();
