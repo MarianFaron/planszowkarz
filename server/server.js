4ceaf16fe7b8f45d416b5852b9327d63c61365b3
xmarianF <xmarianF@gmail.com>
2017-06-03 14:21:07 +0200

    Deploy project to heroku

--- server/server.js
+++ server/server.js
@@ -66,6 +66,19 @@ app.use('/app', avatarUploadRoute);
 app.use('/app', transactionsRoute);
 app.use('/app', notificationsRoute);
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
