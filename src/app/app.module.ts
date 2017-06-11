6c2f4a72591c9749bc71bc775c3bcacd961d2a4f
Aleksander Pośka <aposka31@gmail.com>
2017-06-11 12:58:15 +0200

    fixed slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,23 @@ const swiper_config:SwiperConfigInterface={
   grabCursor: true,
   nextButton: '.swiper-button-next',
   prevButton: '.swiper-button-prev',
+  breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is <= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is <= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 }
 
 @NgModule({
