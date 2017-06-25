9669b26f1de1c3e49920d9089be4ce2d2c9848d7
xmarianF <xmarianF@gmail.com>
2017-06-26 01:02:57 +0200

    Prepare view of games exchange

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -21,7 +21,10 @@ export class ExchangeComponent implements OnInit {
 
   errorMessage: string;
   status: string;
-  recipientGame: UserGame;  
+
+  recipientID: string;
+  recipientGame: UserGame; 
+  recipientInfo: UserInfo;
   senderGames: UserGame[];
   senderInfo: UserInfo;
 
@@ -36,40 +39,54 @@ export class ExchangeComponent implements OnInit {
 
   ngOnInit() {
   	if(localStorage.getItem('currentUser')) {
-      this.getCurrentUserGames();
-      this.getCurrentUserInfo();
+      this.getSenderUserGames();      
     }
-
-  	// Pobierz grę odbiorcy
-    let id = this.activeRoute.snapshot.params['_id'];
-    this.exchangeService.getRecipientGame(id)
-                        .subscribe(
-                            recipientGame => {
-                            	this.recipientGame = recipientGame;
-                            },
-                            error => this.errorMessage = <any>error);
+  	this.getRecipientUserGame();    
   }
 
   // Pobierz gry nadawcy
-  getCurrentUserGames() {
+  getSenderUserGames() {
     var id = this.appService.getCurrentUser()._id;
     this.exchangeService.getSenderGames(id)
                         .subscribe(
                             senderGames => {
                             	this.senderGames = senderGames;
                         	},
-                        	error => this.errorMessage = <any>error);
+                        	error => this.errorMessage = <any>error);  
+    this.getSenderUserInfo();  
   }
 
   // Pobierz dane nadawcy
-  getCurrentUserInfo(){
+  getSenderUserInfo(){
     var id = this.appService.getCurrentUser()._id;
-
     this.userInfoService.getUser(id)
                         .subscribe(
                             userInfo => {
                           		this.senderInfo = userInfo;
                         	},
-                        	error => this.errorMessage = <any>error);
+                        	error => this.errorMessage = <any>error);    
+  }
+
+  // Pobierz grę odbiorcy
+  getRecipientUserGame(){
+    let gameID = this.activeRoute.snapshot.params['_id'];
+    this.exchangeService.getRecipientGame(gameID)
+                        .subscribe(
+                            recipientGame => {
+                              this.recipientGame = recipientGame;
+                              this.recipientID = this.recipientGame.userID;
+                            },
+                            error => this.errorMessage = <any>error);
+    this.getRecipientUserInfo(this.recipientID);
+  }
+
+  // Pobierz dane odbiorcy
+  getRecipientUserInfo(id: string){    
+    this.userInfoService.getUser(id)
+                        .subscribe(
+                            userInfo => {
+                              this.recipientInfo = userInfo;
+                          },
+                          error => this.errorMessage = <any>error);    
   }
 }
